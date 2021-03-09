a1db9bb952c1bbe5694a4c721678dad7b6f16d9f
SaifulHub <si.saiful.sist@gmail.com>
2021-03-09 22:14:03 +0600

    9 march some edition done

--- Myportfolio/personal/views.py
+++ Myportfolio/personal/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_sreen_view(request):
-    print(request.headers)
-    return render(request, "base.html", {})
+    context = {}
+    # context['some_string'] = "I am passing this string from views.py to home.html"
+    # context['some_num'] = 12345
+
+    list_of_values = []
+    list_of_values.append("first_entry")
+    list_of_values.append("second_entry")
+    list_of_values.append("third_entry")
+    list_of_values.append("fourth_entry")
+    context['list_of_values'] = list_of_values
+
+    return render(request, "home.html", context)
