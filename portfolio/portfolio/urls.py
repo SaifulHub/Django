d4903b610d6e29a6d293ed127c43cc79b89f6215
SaifulHub <si.saiful.sist@gmail.com>
2021-03-30 10:09:35 +0600

    new

--- portfolio/portfolio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include  # have to add 'include' to add other apps
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('employee/',include('employee.urls')),
-    path('login/',include('employee.urls'))
-]
