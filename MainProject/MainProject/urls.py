ad25805b493bfaf4748271141a7f8566398cd315
SaifulHub <si.saiful.sist@gmail.com>
2021-03-30 09:55:59 +0600

    changes occured by mw

---
+++ MainProject/MainProject/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path,include  # have to add 'include' to add other apps
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('MainApp.urls')),
+]
